a926a2d8cf5d1ab616c461f335ef8e704e8e91df
niziyal <alenkan@yandex.ru>
2022-11-21 16:19:37 +0300

    added display list of recipes

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -12,6 +12,16 @@ public class Ingredient {
     private BigDecimal amount;
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
+    @ManyToOne
+    private Recipe recipe;
+    public Ingredient() {
+    }
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
 
     public UnitOfMeasure getUom() {
         return uom;
@@ -20,9 +30,6 @@ public class Ingredient {
     public void setUom(UnitOfMeasure uom) {
         this.uom = uom;
     }
-
-    @ManyToOne
-    private Recipe recipe;
 
     public Long getId() {
         return id;
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,33 +1,51 @@
 package guru.springframework.domain;
+
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Recipe {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
+
     @Lob
     private Byte[] image;
+
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getDescription() {
         return description;
@@ -99,14 +117,6 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public Set<Ingredient> getIngredients() {
