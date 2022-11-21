94f657447aa9bf7067e561ba8c3a5728f9bf93e0
niziyal <alenkan@yandex.ru>
2022-11-21 13:00:14 +0300

    added enumeration

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -19,6 +19,8 @@ public class Recipe {
     private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -100,5 +102,21 @@ public class Recipe {
 
     public Long getId() {
         return id;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
