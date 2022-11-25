1cb9445a74c5159560e8702c9fcc368fecd26781
niziyal <alenkan@yandex.ru>
2022-11-25 11:35:20 +0300

    added 'delete ingredient' functionality

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -4,7 +4,7 @@ import guru.springframework.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
-
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 
 }
